4caff9a9c9c472c96db12819c960cd95550b8102
Peter Friese <peter@peterfriese.de>
2020-10-08 13:40:50 +0200

    Initial Commit

---
+++ Colors/ColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorsApp.swift
+//  Colors
+//
+//  Created by Peter Friese on 08/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct ColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Colors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Colors
+//
+//  Created by Peter Friese on 08/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
